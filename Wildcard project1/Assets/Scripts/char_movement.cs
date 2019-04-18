010bd0662c54621dbf72ff5e3851cb759ba8cf8e
Dane Carstens <dcarsten@uci.edu>
2019-04-17 22:42:15 -0700

    Created a scene, character movement works

---
+++ Wildcard project1/Assets/Scripts/char_movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class char_movement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal") * speed;
+        float vert = Input.GetAxis("Vertical") * speed;
+        rb.velocity = new Vector3(horiz, vert);
+    }
+}
