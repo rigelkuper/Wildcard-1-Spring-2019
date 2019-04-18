6af4f619ee95c575ed9972067d39f721399c8895
Matthew Fernandez <mattfer98@gmail.com>
2019-04-18 12:23:56 -0700

    Created base character health and player health scripts

---
+++ Wildcard project1/Assets/Scripts/char_health.cs
@@ -0,0 +1,39 @@
+﻿// Please let Matthew know if you modify this script.
+
+// Base character health script
+// Use this for health of all characters
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class char_health : MonoBehaviour
+{
+
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public int getHealth()
+    {
+        return currentHealth;
+    }
+
+    public void setHealth(int newHealth)
+    {
+        currentHealth = newHealth;
+    }
+
+    public void decraseHealth(int amount)
+    {
+        if (currentHealth - amount <= 0){
+            currentHealth = 0;
+        } else {
+            currentHealth -= amount;
+        }
+    }
+
+
+}
---
+++ Wildcard project1/Assets/Scripts/playerLife.cs
@@ -0,0 +1,53 @@
+﻿// Please let Matthew know if you modify this script.
+
+// Handles player damage, iframes, killing player, etc.
+// Make sure to attach char_health component to player object
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerLife : MonoBehaviour
+{
+    public int maxHealth = 20;
+    
+    char_health playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GetComponent<char_health>();
+        playerHealth.setHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(playerHealth.getHealth());
+        if (playerHealth.getHealth() == 0){
+            killPlayer();
+        }
+    }
+
+    public void damagePlayer(int amount)
+    {
+        playerHealth.decraseHealth(amount);
+    }
+
+    public void healPlayer(int amount)
+    {
+        int newHealth = playerHealth.getHealth() + amount;
+        if (newHealth >= maxHealth){
+            playerHealth.setHealth(maxHealth);
+        } else {
+            playerHealth.setHealth(newHealth);
+        }
+    }
+
+    public void killPlayer()
+    {
+        Debug.Log("Player died!");
+        Destroy(gameObject);
+    }
+
+    
+}
