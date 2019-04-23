1667ad4c091ede0bd348c3b26eaacf39a47af038
Matthew Fernandez <mattfer98@gmail.com>
2019-04-23 12:21:43 -0700

    Zombie health & hurtbox for player

---
+++ Wildcard project1/Assets/Scripts/enemy_hurtbox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_hurtbox : MonoBehaviour
+{
+    [SerializeField] int damageAmount = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Hit something");
+        if (collision.gameObject.tag == "Player"){
+            collision.gameObject.GetComponent<playerLife>().damagePlayer(damageAmount);
+        }
+    }
+
+
+}
