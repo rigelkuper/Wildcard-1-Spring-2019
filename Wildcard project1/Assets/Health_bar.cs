46e96f77a5148e2f5a767e6f0dd4663889df7c63
Matthew Fernandez <mattfer98@gmail.com>
2019-05-02 12:24:05 -0700

    Camera and health bar

---
+++ Wildcard project1/Assets/Health_bar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health_bar : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject player;
+    Slider barSlider;
+    float normalized;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        barSlider = GetComponent<Slider>();
+        normalized = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        normalized = (float) player.GetComponent<char_health>().getHealth() / (float) player.GetComponent<playerLife>().maxHealth;
+        barSlider.normalizedValue = normalized;
+    }
+}
