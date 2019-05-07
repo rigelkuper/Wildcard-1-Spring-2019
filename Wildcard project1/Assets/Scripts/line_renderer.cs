4c7f75e7dfd708fab79e7436957494baba752318
Dane Carstens <dcarsten@uci.edu>
2019-05-07 12:17:27 -0700

    added random directions to guns, variable number of shots

---
+++ Wildcard project1/Assets/Scripts/line_renderer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class line_renderer : MonoBehaviour
+{
+    public Transform fire_point;
+    public LineRenderer shot;
+    public float shot_distance = 5f;
+    public Vector3 length;
+    public float seconds = 100.5f;
+    public float time_since = 0f;
+    public float start = 0;
+    public float test_scalar = 1f;
+    public float accuracy = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float scalar = Random.Range(-accuracy, accuracy);
+        start = Time.time;
+        shot.SetPosition(0, new Vector3(0, 0, 0));
+        shot.SetPosition(1, new Vector3(0, 0, 0));
+        shot.startWidth = 0;
+        shot.endWidth = 0;
+        shot.startWidth = .1f;
+        shot.endWidth = .1f;
+        Debug.Log(fire_point.right);
+        RaycastHit2D hit_info = Physics2D.Raycast(fire_point.position, fire_point.right + fire_point.up * (0.2f * scalar), shot_distance);
+        shot.SetPosition(0, fire_point.position);
+        if (hit_info.collider != null)
+        {
+            test_health health = hit_info.transform.GetComponent<test_health>();
+            health.health -= 1;
+            shot.SetPosition(1, hit_info.point);
+        }
+        else
+        {
+            length = (fire_point.up);
+            shot.SetPosition(1, (fire_point.position + fire_point.right * shot_distance + fire_point.up * scalar));
+        }
+        StartCoroutine(wait());
+        
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(.5f);
+        shot.SetPosition(0, new Vector3(0, 0, 0));
+        shot.SetPosition(1, new Vector3(0, 0, 0));
+        shot.startWidth = 0;
+        shot.endWidth = 0;
+        
+    }
+    
+}
--- Wildcard project1/Assets/Scripts/raycast_shoot.cs
+++ Wildcard project1/Assets/Scripts/raycast_shoot.cs
@@ -10,6 +10,8 @@ public class raycast_shoot : MonoBehaviour
     public Vector3 length;
     public float seconds = 100.5f;
     public float time_since = 0f;
+    public GameObject line;
+    public int num_shots = 1;
     void Start()
     {
         shot.SetPosition(0, new Vector3(0, 0, 0));
@@ -19,39 +21,29 @@ public class raycast_shoot : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && (time_since <= 0))
+        if (Input.GetButton("Fire1") && (time_since <= 0))
         {
-            Shoot();
-            time_since = seconds;
+            for (int i = 0; i < num_shots; i++)
+            {
+                Shoot();
+                time_since = seconds;
+            }
+            
         }
         time_since -= Time.deltaTime;
     }
     void Shoot()
     {
-        shot.startWidth = .1f;
-        shot.endWidth = .1f;
-        RaycastHit2D hit_info = Physics2D.Raycast(fire_point.position, fire_point.right, shot_distance);
-        shot.SetPosition(0, fire_point.position);
-        if (hit_info.collider != null)
-        {
-            test_health health = hit_info.transform.GetComponent<test_health>();
-            health.health -= 1;
-            shot.SetPosition(1, hit_info.point);
-        }
-        else
-        {
-            length = (fire_point.up);
-            shot.SetPosition(1, fire_point.position + fire_point.right * shot_distance);
-        }
-        StartCoroutine(wait());
+        GameObject go;
+        go = Instantiate(line) as GameObject;
+        
+        
+        StartCoroutine(wait(go));
     }
-    IEnumerator wait()
+    IEnumerator wait(GameObject go)
     {
         yield return new WaitForSeconds(.5f);
-        shot.SetPosition(0, new Vector3(0, 0, 0));
-        shot.SetPosition(1, new Vector3(0, 0, 0));
-        shot.startWidth = 0;
-        shot.endWidth = 0;
+        Destroy(go);
     }
     
 }
