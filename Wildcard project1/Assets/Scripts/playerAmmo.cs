7038e91bb047499a036d4ba825a79e735433de2a
Matthew Fernandez <mattfer98@gmail.com>
2019-05-14 12:17:31 -0700

    Started on ammo & mattZombie prefab

---
+++ Wildcard project1/Assets/Scripts/playerAmmo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAmmo : MonoBehaviour
+{
+    public int startingAmmo = 0;
+
+    int currentAmmo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAmmo = startingAmmo;
+    }
+
+    public int getCurrentAmmo()
+    {
+        return currentAmmo;
+    }
+
+    public void increaseAmmo(int amount)
+    {
+        currentAmmo += amount;
+    }
+
+    public void decreaseAmmo(int amount)
+    {
+        if ((currentAmmo - amount) <= 0){
+            currentAmmo = 0;
+        } else {
+            currentAmmo -= amount;
+        }
+    }
+}
