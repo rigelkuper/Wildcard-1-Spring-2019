5c54461b444ac30f968649b64001a8b9d7cae019
Matthew Fernandez <mattfer98@gmail.com>
2019-05-14 12:33:10 -0700

    Started on ammoPickup.

---
+++ Wildcard project1/Assets/Scripts/ammoPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ammoPickup : MonoBehaviour
+{
+    public int increaseAmount = 5;
+
+    void onTriggerEnter2D(Collider2D other){
+        if (other.gameObject.tag == "Player" && !other.isTrigger){
+            other.gameObject.GetComponent<playerAmmo>().increaseAmmo(increaseAmount);
+        }
+
+    }
+}
