b6df6b839b136b19a946f5d06de8d221884abb7e
Dane Carstens <dcarsten@uci.edu>
2019-05-14 12:11:44 -0700

    Gun switching now works

--- Wildcard project1/Assets/Scripts/gun_manager.cs
+++ Wildcard project1/Assets/Scripts/gun_manager.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class gun_manager : MonoBehaviour
 {
+    public GameObject current_gun;
+    public int current_index = 0;
+    public Transform parent;
+
+
+
+    public GameObject gun1;
+    public GameObject gun2;
+    public GameObject gun3;
+    public GameObject gun4;
+    List<GameObject> temp_list;
+    List<GameObject> gun_list;
+
     // Start is called before the first frame update
     void Start()
     {
+        temp_list = new List<GameObject>();
+        gun_list = new List<GameObject>();
+        temp_list.Add(gun1);
+        temp_list.Add(gun2);
+        temp_list.Add(gun3);
+        temp_list.Add(gun4);
+        //yes I know it is hardcoded and ugly. Sue me
+        foreach (GameObject go in temp_list)
+        {
+            if (go != null)
+            {
+                gun_list.Add(go);
+            }
+            
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("change_gun"))
+        {
+            current_index += (int)Input.GetAxis("change_gun");
+        }
+
+        if (current_index < 0)
+        {
+            current_index = gun_list.Count;
+        }
+        if (current_index >= gun_list.Count)
+        {
+            current_index = 0;
+        }
+        current_gun = gun_list[current_index];
+        
+        
+        if (Input.GetButtonDown("change_gun"))
+        {
+            foreach (Transform child in transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+
+            //current_gun.transform.SetParent(parent);
+            var gun = Instantiate(current_gun, parent.transform);
+            gun.transform.SetParent(parent);
+            Debug.Log(current_gun);
+        }
         
     }
 }
