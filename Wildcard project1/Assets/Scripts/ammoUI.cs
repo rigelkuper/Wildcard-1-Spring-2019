75de09fc7cf8164f82109e21ab1ad5d904cc96a8
Matthew Fernandez <mattfer98@gmail.com>
2019-05-16 12:27:39 -0700

    Ammo pickup/UI; testing new prefabs in MattScene

---
+++ Wildcard project1/Assets/Scripts/ammoUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ammoUI : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = player.GetComponent<playerAmmo>().getCurrentAmmo().ToString();
+    }
+}
