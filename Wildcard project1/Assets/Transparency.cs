40992c32cad20dfdded91af7b4b0a4fe4953b312
cmlguy <44219118+cmlguy@users.noreply.github.com>
2019-05-23 13:12:36 -0700

    House Transparency

---
+++ Wildcard project1/Assets/Transparency.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparency : MonoBehaviour
+{
+    private SpriteRenderer Renderer;
+
+    private Color defaultColor;
+    private Color fadedColor;
+
+    public float alpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+
+        defaultColor = Renderer.color;
+        fadedColor = defaultColor;
+        fadedColor.a = alpha;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Renderer.color = fadedColor;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Renderer.color = defaultColor;
+        }
+    }
+
+}
